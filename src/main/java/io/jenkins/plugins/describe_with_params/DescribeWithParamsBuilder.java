ff1dd1537e3437f206a1f37c3b742b56f8b22571
liumazi <71892967@qq.com>
2020-10-08 12:51:17 +0800

    Delete no useful symbol

--- src/main/java/io/jenkins/plugins/describe_with_params/DescribeWithParamsBuilder.java
+++ src/main/java/io/jenkins/plugins/describe_with_params/DescribeWithParamsBuilder.java
@@ -73,7 +73,6 @@ public class DescribeWithParamsBuilder extends Builder {
         return true;
     }
 
-    @Symbol("greet")
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
